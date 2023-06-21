32a47254ea120009ba6ddfd88d3751b3d9984ae8
Sunil Park <sunilpark1129@gmail.com>
2023-06-21 19:35:57 -0400

    Add Question Component

---
+++ src/modals/Question.tsx
@@ -0,0 +1,56 @@
+import { useStore } from "../store";
+export default function Question() {
+  const setModal = useStore((store) => store.setModal);
+  return (
+    <div
+      className="modal__content modal__content--question"
+      onClick={() => setModal(null)}
+    >
+      <div className="modal__question modal__question--floor">
+        <div>
+          <p>Vocab Storage Stage</p>
+        </div>
+      </div>
+      <div className="modal__question modal__question--plus">
+        <div>
+          <p>Add Vocab</p>
+        </div>
+      </div>
+      <div className="modal__question modal__question--delete">
+        <div>
+          <p>Delete Vocab</p>
+        </div>
+      </div>
+      <div className="modal__question modal__question--edit">
+        <div>
+          <p>Edit Vocab</p>
+        </div>
+      </div>
+      <div className="modal__question modal__question--help">
+        <div>
+          <p>Help</p>
+        </div>
+      </div>
+      <div className="modal__question modal__question--flip">
+        <div>
+          <p>Flip Vocab</p>
+        </div>
+      </div>
+      <div className="modal__question modal__question--shuffle">
+        <div>
+          <p>Shuffle Stage Items</p>
+        </div>
+      </div>
+      <div className="modal__question modal__question--up">
+        <div>
+          <p>Upgrade Vocab to Next Stage</p>
+        </div>
+      </div>
+      <div className="modal__question modal__question--down">
+        <div>
+          <p>Downgrade Vocab to Previous Stage</p>
+        </div>
+      </div>
+    </div>
+  );
+}
